0408d5a201cc62342f1cd2d9bb822cd94987a2c8
dele1972 <ostfalia@d.lederich.de>
2020-09-22 18:47:43 +0200

    some config and git ignore

--- config/AppConfig.php
+++ config/AppConfig.php
@@ -1,5 +1,11 @@
 <?php
     class AppConfig {
         static $htmlFileInputPath = '../samples/';
+
+        static $db_host = "localhost";
+        static $db_name = "store-web-data";
+        static $db_user = "root";
+        static $db_password = "";
+        static $db_collation = "utf8mb4_unicode_ci";
     }
 ?>
